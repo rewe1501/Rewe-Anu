dc83ab6b63cf8d3372a0af354654f63bb6e115ed
rewe1501 <168255947+rewe1501@users.noreply.github.com>
2024-04-27 13:19:34 +0700

    Update exceptions.py

--- Rewe/exceptions.py
+++ Rewe/exceptions.py
@@ -6,7 +6,7 @@
 # <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 """
-Exceptions which can be raised by Kazu Ubot Itself.
+Exceptions which can be raised by Rewe Ubot Itself.
 """
 
 
