bd8d11b794b36086dc009c5447b81f2766b66678
rewe1501 <168255947+rewe1501@users.noreply.github.com>
2024-04-27 13:18:58 +0700

    Update loader.py

--- Rewe/loader.py
+++ Rewe/loader.py
@@ -62,7 +62,7 @@ class Loader:
                 self._logger.error(f"{plugin}: '{er.name}' tidak terpasang!")
             except Exception as exc:
                 modl = None
-                self._logger.error(f"Kazu - {self.key} - ERROR - {plugin}")
+                self._logger.error(f"Rewe - {self.key} - ERROR - {plugin}")
                 self._logger.exception(exc)
             if callable(after_load):
                 if func == import_module:
